bf66f751d5d9447172fd84bd5d48d49510afb68e
Christian Manzaraz <magnusmanz@gmail.com>
2022-12-17 21:15:12 -0300

    creating the Emoji model

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,13 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Christian Manzaraz on 17/12/2022.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String, name: String , description: String , usage: String
+    
+}
