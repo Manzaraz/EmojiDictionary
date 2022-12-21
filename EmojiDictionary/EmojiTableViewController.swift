ebdbbbf306c114c4087413f2952ad7ce24a58cc2
Christian Manzaraz <magnusmanz@gmail.com>
2022-12-21 00:00:50 -0300

    Edit Table Views

--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -103,17 +103,18 @@ class EmojiTableViewController:  UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
+            emojis.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+//        } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     
     // Override to support rearranging the table view.
@@ -124,7 +125,7 @@ class EmojiTableViewController:  UITableViewController {
 
     // Override to be able  to reorder items, not delet them
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .none
+        return .delete
     }
     
     // Reload Data
